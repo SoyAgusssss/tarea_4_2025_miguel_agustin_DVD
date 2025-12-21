3448ff219810ef4609955a0b654e2fa0dff01ae4
Miguel_Agustin <miguel-agus1995@hotmail.com>
2025-12-21 12:07:42 +0100

    clase Movie

---
+++ Movie.js
@@ -0,0 +1,48 @@
+import Production from "./Production.js";
+
+class Movie extends Production {
+    #resource
+    #locations
+    constructor(title, nationality, publication, sypnopsis, image, resource = null, locations = []) {
+        super(resource, locations)
+        this.resource = resource
+        this.locations = locations
+    }
+
+    get resource() {
+        return this.#resource
+    }
+
+    set resource(v) {
+        if(v !== null && !(v instanceof Resource)) {
+            throw new Error("Error en el recurso. Debe ser una instancia de Resource")
+        }
+        this.#resource = v
+    }
+
+    get locations() {
+        return this.#locations
+    }
+
+    set locations(v) {
+        if(!Array.isArray(v)) {
+            throw new Error("La localización debe de ser un Array")
+        }
+
+        for(const c of v) {
+            if(!(c instanceof Coordinate)) {
+                throw new Error("La localización debe ser una instancia de Coordinate")
+            }
+        }
+        this.#locations = v
+    }
+}
+
+/*
+1.5. Objeto Movie
+Representa un recurso película que podremos reproducir en el sistema. Hereda de Producción.
+Propiedad Tipo Obligatorio Descripción
+resource Resource No Recurso con el contenido de la película.
+locations [Coordinate] No Array con diferentes ubicaciones donde
+transcurre la película.
+*/
--- pruebas.js
+++ pruebas.js
@@ -2,6 +2,7 @@ import Person from "./Person.js"
 import Category from "./Category.js"
 import Resource from "./Resource.js"
 import Production from "./Production.js"
+import Movie from "./Movie.js"
 
 const naci = new Date(2025,11,12)
 
