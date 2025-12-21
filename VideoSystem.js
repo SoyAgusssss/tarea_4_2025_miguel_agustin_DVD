26e5903cc163cdcf7fa16e850f807f6993ce9081
Miguel_Agustin <miguel-agus1995@hotmail.com>
2025-12-21 17:59:54 +0100

    añadir y eliminar usuarios y producciones

--- VideoSystem.js
+++ VideoSystem.js
@@ -1,4 +1,6 @@
 import Category from "./Category.js"
+import User from "./User.js"
+import Production from "./Production.js"
 
 class VideoSystem {
     static #instance
@@ -65,4 +67,70 @@ class VideoSystem {
             }
             return this.#categories.size
         }
+
+        get users() {
+            return this.#users.values()
+        }
+
+        addUser(...users) {
+            for(const u of users) {
+                if (u === null || !(u instanceof User)) {
+                    throw new Error("El usuario no puede ser null o no es un objeto User");
+                }
+                if(this.#users.has(u.username)) {
+                    throw new Error("El nombre ya existe")
+                }
+                for(const mal of this.#users.values()) {
+                    if(mal.mail === u.mail) {
+                        throw new Error("El correo ya existe")
+                    }
+                }
+                this.#users.set(u.username, u)  
+            }
+            return this.#users.size
+        }
+
+        removeUser(...users){
+            for(const u of users) {
+                if(u === null || !(u instanceof User)) {
+                    throw new Error("El usuario no puede ser null o no ser un susuario")
+                }
+                if(!this.#users.has(u.username)) {
+                    throw new Error("El usuario no existe")
+                }
+                this.#users.delete(u.username)
+            }
+            return this.#users.size
+        }
+
+        get productions() {
+            return this.#productions.values()
+        }
+
+        addProductions(...productions){
+            for(const p of productions) {
+                if(p === null || !(p instanceof Production)) {
+                    throw new Error("La producción no puede ser null o no ser una Producción")
+                }
+                if(this.#productions.has(p.title)) {
+                    throw new Error("Ya existe una producción")
+                }
+                this.#productions.set(p.title, p)
+            }
+            return this.#productions.size
+        }
+
+        removeProductions(...productions) {
+            for(const p of productions) {
+                if(p  === null || !(p instanceof Production)) {
+                    throw new Error("No puede ser null o no ser una producción")
+                }
+                if(!this.#productions.has(p.title)) {
+                    throw new Error("Error la producción debe de existir")
+                }
+                this.#productions.delete(p.title)
+            }
+        }
+
+
 }
